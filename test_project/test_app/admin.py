5abc3916f74553f35c4dc2dcc9c1254f75681d85
Shurik <stu.shurik@gmail.com>
2014-05-23 16:09:41 +0300

    search in admin

--- test_project/test_app/admin.py
+++ test_project/test_app/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from test_app.models import Employee, Department
 
-admin.site.register(Employee)
-admin.site.register(Department)
+
+class DepartmentAdmin(admin.ModelAdmin):
+    search_fields = ('name', )
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    search_fields = ('first_name', 'last_name', 'department__name')
+
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Employee, EmployeeAdmin)
 
